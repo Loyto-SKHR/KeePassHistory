43b3febfbf4bb8236375f3941a5a996fc4c93316
Loyto_SKHR <contact@skhr.fr>
2024-08-06 17:38:39 +0200

    add form

--- src/KeePassHistoryExt.cs
+++ src/KeePassHistoryExt.cs
@@ -1,7 +1,9 @@
 using System;
-using System.Collections.Generic;
 
+using System.Windows.Forms;
 using KeePass.Plugins;
+using KeePassHistory.src;
+
 
 namespace KeePassHistory
 {
@@ -19,5 +21,36 @@ namespace KeePassHistory
 		public override void Terminate()
 		{
 		}
-	}
+
+        public override ToolStripMenuItem GetMenuItem(PluginMenuType pluginMenuType)
+		{
+			if (pluginMenuType != PluginMenuType.Main) return null;
+
+			ToolStripMenuItem tsmi = new ToolStripMenuItem("History");
+
+            //View
+            ToolStripMenuItem tsmiView = new ToolStripMenuItem("View");
+			tsmiView.Click += this.OnMenuViewClicked;
+			tsmi.DropDownItems.Add(tsmiView);
+
+            //reset
+            ToolStripMenuItem tsmiReset = new ToolStripMenuItem("Reset");
+            tsmiView.Click += this.OnMenuResetClicked;
+            tsmi.DropDownItems.Add(tsmiReset);
+
+            return tsmi;
+        }
+
+        private void OnMenuViewClicked(object sender, EventArgs e)
+        {
+			var selectDate = new SelectDate(this.m_host);
+
+			selectDate.ShowDialog(this.m_host.MainWindow);
+        }
+
+        private void OnMenuResetClicked(object sender, EventArgs e)
+        {
+
+        }
+    }
 }
---
+++ src/SelectDate.Designer.cs
@@ -0,0 +1,54 @@
+﻿using KeePass.Plugins;
+using System.Windows.Forms;
+
+
+namespace KeePassHistory.src
+{
+    public partial class SelectDate : Form
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        private IPluginHost m_host = null;
+
+        public SelectDate(IPluginHost m_host)
+        {
+
+            InitializeComponent();
+            this.m_host = m_host;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SelectDate));
+            this.SuspendLayout();
+            // 
+            // SelectDate
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "SelectDate";
+            this.Text = "Select date";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
